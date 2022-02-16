76041c142ab63447022dacf236fc6878e52c25c9
zbbxh <hmyyqxi@gmail.com>
2022-02-16 10:32:40 -0500

    update

--- Lab-1/file_in.cpp
+++ Lab-1/file_in.cpp
@@ -1,81 +1,231 @@
 //Huimin Chen
-//CISC 3142
-// This file shows basic functions of reading in a CSV file using C++98
- 
+//CISC-3142
+//Lab 1
+
 /* Homework Objective: fix this program to match the requirements listed: 
   1. successfully reads all lines of the input file
   2. calculate the average price per brand, average price per category 
   3. writes to an output file
   4. for each unique year, list the count of skus and also print out the skus as a list i.e. 2000 (3): 111, 211, 311 and make a new line per year.
-
-All programs must be able to compile in C++98 standard (the default version on Linux Machine). That means you cannot use the new features of C++11! :(
 */
 
 #include <iostream>
-#include <string>
+#include <iterator>
+#include <fstream>
+#include <map>
+#include <numeric>
 #include <sstream>
 #include <vector>
-#include <fstream>
+using namespace std;
 
-using namespace std; 
+//replace all "," to space
+void replace(string&line, char at, char with) {
+    for(int i=0; i<line.length(); i++) {
+        if(line[i] == at)  
+            line[i] = with;
+    }
+}
 
-int main() {    
-// define variables
-  string sku, brand, category, year, price; 
-  vector<int>vSKU;
-  vector<string>vBrand;
-  vector<string>vCategory;
-  vector<int>vYear;
-  vector<float>vPrice;
-
-  ifstream in_stream;
-  in_stream.open("data.csv"); //opening the file.
-
-  if (!in_stream.fail()) { //if the file is open
-
-    string line = "";
-
-    while (getline(in_stream,line)) { //while the end of file is NOT reached
-
-     // Fields: sku,brand,category,year,price
-     getline(in_stream, sku, ',');
-     stringstream ssku(sku); 
-     int iSKU = 0;
-     ssku >> iSKU;
-     vSKU.push_back(iSKU);
-
-     getline(in_stream, brand, ',');
-     vBrand.push_back(brand);
-
-     getline(in_stream, category, ',');
-     vCategory.push_back(category);
-
-     getline(in_stream, year, ','); 
-     stringstream syear(year);
-     int iYear;
-     syear >> iYear;
-     vYear.push_back(iYear);
-
-     getline(in_stream, price, '\n'); 
-     stringstream sprice(price);
-     float fPrice;
-     sprice >> fPrice;
-     vPrice.push_back(fPrice);
-     
+//calculate the average price per brand
+// This function calculates the average prices per brand
+void calculateAveragePerBrand(vector<string> vBrand, vector<float> vPrice, ofstream& out_stream)
+{
+    // Declaring a map to store the prices band wise
+    map<string,vector<float> > brandMap;
+    
+    // Storing the prices band wise
+    for(int i = 0; i < vBrand.size(); i++)
+    {
+        brandMap[vBrand[i]].push_back(vPrice[i]);
     }
 
-    in_stream.close(); //closing the file cout << "Number of entries: " << i-1 << endl;
+    // Writing the headers to the output file
+    out_stream << "\tBrand\tAverage\n";
+    
+    // Declaring an iterator for map to iterate over the map
+    map<string,vector<float> >::iterator it;
+    
+    // Iterating over the map
+    for(it = brandMap.begin(); it != brandMap.end(); ++it)
+    {
+        // Storing the current brand
+        string curBrand = it->first;
+        // Storing the prices of current brand
+        vector<float> curPrices = it->second;
+        // Storing the sum of prices of current brand
+        float brandSum = accumulate(curPrices.begin(), curPrices.end(),0.0);
+        // Calculating the average of prices of current brand
+        float average = brandSum / curPrices.size();
+        
+        // Writing the brand name and it's average to the output file
+        out_stream << "\t" << curBrand << "\t" << average << "\n";
+    }
+    // Writing a newline to the output file
+    out_stream << endl;
+}
 
-   } else {
-    cout << "Unable to open file"; 
-   }
- 
-  //output values 
-  cout << "SKU" << "\t" << "Brand" << "\t" << "Year" << "\t" << "Price" << endl; 
+// This function calculates the average prices per category
+void calculateAveragePerCategory(vector<string> vCategory, vector<float> vPrice, ofstream& out_stream)
+{
+    // Declaring a map to store the prices category wise
+    map<string,vector<float> > categoryMap;
+    // Storing the prices category wise
+    for(int i = 0; i < vCategory.size(); i++)
+    {
+        categoryMap[vCategory[i]].push_back(vPrice[i]);
+    }
 
-  for (int j = 0; j < vSKU.size(); j++) {
-    cout << vSKU[j] << "\t" << vBrand[j] << "\t" << vYear[j] << "\t" << vPrice[j] << endl;
-  }
+    // Writing the headers to the output file
+    out_stream << "\tCategory\tAverage\n";
+    
+    // Declaring an iterator for map to iterate over the map
+    map<string,vector<float> >::iterator it;
+    
+    // Iterating over the map
+    for(it = categoryMap.begin(); it != categoryMap.end(); it++)
+    {
+        // Storing the current category
+        string curCategory = it->first;
+        // Storing the prices of current category
+        vector<float> curPrices = it->second;
+        // Storing the sum of prices of current category
+        float brandSum = accumulate(curPrices.begin(), curPrices.end(),0.0);
+        // Calculating the average of prices of current category
+        float average = brandSum / curPrices.size();
 
-  cout << endl;
+        // Writing the category name and it's average to the output file
+        out_stream << "\t" << curCategory << "\t" << average << "\n";
+    }
+    // Writing a newline to the output file
+    out_stream << endl;
+}
+
+void calculateSKUCount(vector<int> vYear,vector<int> vSKU, ofstream& out_stream)
+{
+    // Declaring a map to store the sku's year wise
+    map<int,vector<int> > yearMap;
+    
+    // Storing the sku's band wise
+    for(int i = 0; i < vYear.size(); i++)
+    {
+        yearMap[vYear[i]].push_back(vSKU[i]);
+    }
+
+    // Writing the headers to the output file
+    out_stream << "\tYear\tSKU's\n";
+    
+    // Declaring an iterator for map to iterate over the map
+    map<int,vector<int> >::iterator it;
+    
+    // Iterating over the map
+    for(it = yearMap.begin(); it != yearMap.end(); it++)
+    {
+        // Writing the year to the output file
+        out_stream << it->first << "\t";
+
+        // Declaring an iterator to iterate over the vector for the current year
+        vector<int>::iterator sku_it;
+
+        // Iterating over sku's for current year
+        for(sku_it = it->second.begin(); sku_it != it->second.end(); sku_it++)
+        {
+            // Writing the sku to the output file
+            out_stream << *sku_it;
+
+            // If this is not the last sku for current year, write a space to the output file
+            if(sku_it != it->second.end())
+            {
+                out_stream << " ";
+            }
+            // Else, write a tab to the output file
+            else
+            {
+                out_stream << "\t";
+            }
+        }
+        // Writing the number of sku's for current year to the output file
+        out_stream << "(Count: " << it->second.size() << ")" << endl;
+    }
+}
+
+int main() {
+     // define variables
+    int SKU;
+    string brand;
+    string category;
+    int year;
+    float price;
+    vector<int> vSKU;
+    vector<string> vBrand;
+    vector<string> vCategory;
+    vector<int> vYear;
+    vector<float> vPrice;
+
+    //read data from file
+    ifstream in_stream;
+    //write data to file
+    ofstream out_stream;
+
+    //opening the input and output file
+    in_stream.open("data.csv");
+    out_stream.open("output.txt");
+
+    //if file open
+    if(!in_stream.fail()) {
+        //store current line that read from file
+        string line;
+
+        // Reading the header line but not using it, because it is not needed
+        getline(in_stream,line);
+
+        //read all data from file until end
+        while(getline(in_stream,line)) {
+            
+            // Replacing commas with spaces
+            replace(line,',',' ');
+
+            //store line to the ss object
+            stringstream ss(line);
+
+            //store values from each column for current row
+            ss>>SKU;
+            ss>>brand;
+            ss>>category;
+            ss>>year;
+            ss>>price;
+
+            //write value into vector
+            vSKU.push_back(SKU);
+            vBrand.push_back(brand);
+            vCategory.push_back(category);
+            vYear.push_back(year);
+            vPrice.push_back(price);
+        }//close file
+        in_stream.close();
+    }else
+    {
+        out_stream<<"Unable to open file";
+    }
+
+    //write value to output file 
+    out_stream << "SKU" << "\t" << "Brand" << "\t" << "Category"<< "\t" << "Year" << "\t" << "Price" << endl;
+
+    // Writing rows read to the output file
+    for (int j = 0; j < vSKU.size(); j++)   {
+        out_stream << vSKU[j] << "\t" << vBrand[j] << "\t" << vCategory[j] << "\t" << vYear[j] << "\t" << vPrice[j] << endl;
+    }// Writing a newline to the output file
+    out_stream << endl;
+    
+    // Calculating the average price per brand and storing them in the output file
+    calculateAveragePerBrand(vBrand,vPrice,out_stream);
+    
+    // Calculating the average price per category and storing them in the output file
+    calculateAveragePerCategory(vCategory,vPrice,out_stream);
+
+    // Calculating the SKU count for each year and storing them in the output file
+    calculateSKUCount(vYear,vSKU, out_stream);
+    
+    // Closing the input and output files
+    in_stream.close();
+    out_stream.close();
 }
